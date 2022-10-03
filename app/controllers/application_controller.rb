5a08d8da468c707af7dfd33363c8650f081412ee
jjcaburrasi <your.email@example.com>
2022-10-03 19:05:51 +0200

    Minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ require 'rest-client'
 class ApplicationController < ActionController::Base
 
     def hello
-        render html: "WORKS!"
+        # render html: "WORKS!"
         url = "http://localhost:3000/api/v1/placements"
         response = RestClient.get(url)
         placements = JSON.parse(response)
