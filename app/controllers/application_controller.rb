598084a16ae8913d33f988f6ed958e8fb00d2b43
jjcaburrasi <your.email@example.com>
2022-10-03 10:30:36 +0200

    Create call to API in root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
+require 'json'
+require 'rest-client'
+
 class ApplicationController < ActionController::Base
+
     def hello
-        render html: "This is the Reporting App"
+        render html: "WORKS!"
+        url = "http://localhost:3000/api/v1/placements"
+        response = RestClient.get(url)
+        placements = JSON.parse(response)
+        
     end
 end
