556a45eb2f54f5e9ae9d87de33a39562563c8421
jjcaburrasi <your.email@example.com>
2022-10-05 12:05:53 +0200

    set logic to API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
     def hello
         # render html: "WORKS!"
-        url = "http://localhost:3000/api/v1/placements"
+        url = "http://localhost:3000/api/v1/reports?api_user=Reporting_app&token=H2SO4plusNaOHequalNaSO4plusH2O"
         response = RestClient.get(url)
         placements = JSON.parse(response)
         
